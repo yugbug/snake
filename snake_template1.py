0ea705d13bd89ab230420e7a034b61b4e5334f36
Guy Nir <yugbug2828@gmail.com>
2018-12-09 18:29:40 +0200

    Adding apple.

--- snake_template1.py
+++ snake_template1.py
@@ -1,55 +1,97 @@
+import random
 from pygame.locals import *
 import pygame
+import time
+
+
+class Apple:
+    x = 0
+    y = 0
+    step = 44
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def draw(self, surface, image):
+        surface.blit(image, (self.x, self.y))
 
 
 class Player:
-    x = 0
-    y = 0
-    speed = 32
+    x = []
+    y = []
+    step = 44
     direction = 0
+    length = 1
+
+    updateCountMax = 2
+    updateCount = 0
+
+    def __init__(self, length):
+        self.length = length
+        for i in range(0, length):
+            self.x.append(0)
+            self.y.append(0)
 
     def update(self):
-        if self.direction == 0:
-            self.x = self.x + self.speed
-        if self.direction == 1:
-            self.x = self.x - self.speed
-        if self.direction == 2:
-            self.y = self.y - self.speed
-        if self.direction == 3:
-            self.y = self.y + self.speed
+
+        self.updateCount = self.updateCount + 1
+        if self.updateCount > self.updateCountMax:
+
+            # update previous positions
+            for i in range(self.length - 1, 0, -1):
+                print
+                "self.x[" + str(i) + "] = self.x[" + str(i - 1) + "]"
+                self.x[i] = self.x[i - 1]
+                self.y[i] = self.y[i - 1]
+
+            # update position of head of snake
+            if self.direction == 0:
+                self.x[0] = self.x[0] + self.step
+            if self.direction == 1:
+                self.x[0] = self.x[0] - self.step
+            if self.direction == 2:
+                self.y[0] = self.y[0] - self.step
+            if self.direction == 3:
+                self.y[0] = self.y[0] + self.step
+
+            self.updateCount = 0
 
     def moveRight(self):
-        self.direction = 0
+        if self.direction != 1:
+            self.direction = 0
 
     def moveLeft(self):
-        self.direction = 1
+        if self.direction != 0:
+            self.direction = 1
 
     def moveUp(self):
-        self.direction = 2
+        if self.direction != 3:
+            self.direction = 2
+
 
     def moveDown(self):
-        self.direction = 3
+        if self.direction != 2:
+            self.direction = 3
 
-
-guy_the_slow_player = Player()
-guy_the_slow_player.x = 1
-
-idan_the_fast_player = Player()
-idan_the_fast_player.speed = 100
+    def draw(self, surface, image):
+        for i in range(0, self.length):
+            surface.blit(image, (self.x[i], self.y[i]))
 
 
 class App:
     windowWidth = 800
     windowHeight = 600
     player = 0
+    apple = 0
 
     def __init__(self):
         self._running = True
         self._display_surf = None
         self._image_surf = None
-        self.player = Player()
-
-        self.player.speed = 10
+        self._apple_surf = None
+        self.player = Player(10)
+        self.apple = Apple(0, 30)  # random.randint (0,1)  )
 
     def on_init(self):
         pygame.init()
@@ -57,18 +99,23 @@ class App:
 
         pygame.display.set_caption('Pygame pythonspot.com example')
         self._running = True
-        self._image_surf = pygame.image.load("photos/snake_block.png").convert()
+        self._image_surf = pygame.image.load("photos/block.png").convert()
+        self._image_surf = pygame.transform.scale(self._image_surf, (40, 30))
+        self._apple_surf = pygame.image.load("photos/apple2.jpeg").convert()
+        self._apple_surf = pygame.transform.scale(self._apple_surf, (40, 30))
 
     def on_event(self, event):
         if event.type == QUIT:
             self._running = False
 
     def on_loop(self):
+        self.player.update()
         pass
 
     def on_render(self):
         self._display_surf.fill((0, 0, 0))
-        self._display_surf.blit(self._image_surf, (self.player.x, self.player.y))
+        self.player.draw(self._display_surf, self._image_surf)
+        self.apple.draw(self._display_surf, self._apple_surf)
         pygame.display.flip()
 
     def on_cleanup(self):
@@ -96,9 +143,10 @@ class App:
 
             if (keys[K_ESCAPE]):
                 self._running = False
-
             self.on_loop()
             self.on_render()
+
+            time.sleep(50.0 / 1000.0);
         self.on_cleanup()
 
 
